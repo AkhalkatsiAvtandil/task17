d59d604a8501886d5f493d393d81914987212ad1
AkhalkatsiAvtandil <akhalkatsiavto@gmail.com>
2026-05-08 01:03:29 +0400

    დავალება 2

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 for (let i = 0; i < 10; i++) {
   console.log("hello from for loop", i);
 }
+console.log("after for loop");
+
+// task2
+let i = 0;
+while (i < 15) {
+  console.log("hello from for loop", i);
+  i++;
+}
