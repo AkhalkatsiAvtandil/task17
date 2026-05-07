a2b10f5444ecc5f099fdfee5a60dff112ebd4a1d
AkhalkatsiAvtandil <akhalkatsiavto@gmail.com>
2026-05-08 02:41:12 +0400

    დავალება 4
    ვერ გამოვიტანე ლუწი რიცხვები

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ do {
   console.log("hello from do while loop", z);
   z++;
 } while (z < 20);
+// task 4
+
+const numbers = [45, 56, 78, 89, 101, 234, 333, 457];
+// console.log(numbers);
+
+for (let i = 0; i < numbers.lenght; i++);
+
+if (numbers[i] % 2 === 0) {
+  console.log(numbers[i]);
+}
+// } else if (numbers[i] % 2 > 0) {
+//   console.log(numbers[i]);
+// }
